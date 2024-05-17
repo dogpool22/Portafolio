bfcc1ef0837cbfbc1f76b9b1f59c4823d810b651
dogpool22 <david.22.ahuite@gmail.com>
2024-05-16 23:00:27 -0500

    skills

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 // scripts.js
-
 // Aquí puedes añadir JavaScript para funcionalidades adicionales si es necesario
+document.addEventListener("DOMContentLoaded", function() {
+    let currentIndex = 0;
+    const images = document.querySelectorAll(".carousel-images img");
+    const totalImages = images.length;
+
+    function showNextImage() {
+        images[currentIndex].classList.remove("visible");
+        currentIndex = (currentIndex + 1) % totalImages;
+        images[currentIndex].classList.add("visible");
+    }
+
+    images[currentIndex].classList.add("visible");
+    setInterval(showNextImage, 3000); // Cambia la imagen cada 3 segundos
+});
+
